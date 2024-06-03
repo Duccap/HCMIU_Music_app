a8daf57085ef8007f8dde2dbac80dda6d8d080f2
Tuan Le <84724694+Tunizrel@users.noreply.github.com>
2024-06-03 07:04:02 +0700

    update login

--- controllers/user.js
+++ controllers/user.js
@@ -6,15 +6,24 @@ exports.renderHomepage = function(req, res) {
 exports.renderLogin = function(req, res) {
     res.render('login');
 }
-exports.loginHandler = function(req, res) {
+exports.checkLogin = async function(req, res) {
     const { username, password } = req.body;
-  
-    console.log('Username:', username);
-    console.log('Password:', password);
 
-    if (username === "test" && password === "password") {
-      return res.json({ success: true, message: 'Login successful!' });
-    } else {
-      return res.status(401).json({ success: false, message: 'Invalid username or password.' });
-    }
+    try{
+      const user = await User.findByUsername(username);
+      if (!user){
+        return res.status(401).json({ success: false, message: 'Invalid username or password.' });
+      }
+      if(!password != password){
+        return res.status(401).json({ success: false, message: 'Invalid username or password.' });
+      }
+      
+      // set user session or token
+      req.session.user = user;
+      res.json({ success: true, message: 'Login successful!', user });
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ success: false, message: 'Server error.' });
+      }
+  
 };
