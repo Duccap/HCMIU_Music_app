b3e42334d480ec224f4216f4992e45a5403309cc
TaViKhang <Khang9a22016@gmail.com>
2024-06-16 09:17:40 +0700

    release-publish
    basics of web player

--- util/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('music_app', 'root', 'secret', {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    logging: false
-});
-
-
-module.exports = sequelize;
